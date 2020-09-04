889deb8591ef0ec8b750825a44c89de7f77b7ce6
Shawn Shadrix <shshadri@ncsu.edu>
2020-09-04 18:34:53 -0400

    defender sidenav alpha

--- src/app/mathhammer/defender/defender.component.ts
+++ src/app/mathhammer/defender/defender.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-defender',
   templateUrl: './defender.component.html',
   styleUrls: ['./defender.component.scss']
 })
-export class DefenderComponent implements OnInit {
+export class DefenderComponent {
+  toughness = '4';
+  wounds = '1';
+  armourSave = '3+';
+  invulnSave = '-';
+  FNPsave = '-';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
 }
