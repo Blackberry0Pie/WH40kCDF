2f7e19db7822b9d0af41646a9fd7102fafd2b287
Shawn Shadrix <shshadri@ncsu.edu>
2020-09-05 00:43:36 -0400

    weapon tabs

--- src/app/mathhammer/attacker/attacker.component.ts
+++ src/app/mathhammer/attacker/attacker.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-attacker',
@@ -6,7 +7,15 @@ import {Component} from '@angular/core';
   styleUrls: ['./attacker.component.scss']
 })
 export class AttackerComponent {
+  tabs = [1, 2, 3, 4, 5, 6, 7, 8];
+  selected = new FormControl(0);
   attackSkill = '4+';
   reroll = 'none';
   rerollOne = false;
+
+  name = '';
+  attacks = 1;
+  strength = 5;
+  AP = '0';
+  damage = 1;
 }
